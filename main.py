d3bf6fb31e2ab6aaa08a6e318ba92a0cb934d827
Vinlagrenouille <vincent.navales@hotmail.fr>
2017-12-21 17:01:02 +0100

    exercice 2 a

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy
 #import sklearn
 
+#EXERCICE 1
 mu = 10
 sigma = 2
 n = 100
@@ -14,4 +15,34 @@ epsi = numpy.random.normal(0, 1, n)
 y = beta0 + (beta1 * x) + epsi
 
 plt.scatter(x,y)
-plt.show()
+#plt.show()
+
+#EXERCICE 2
+def avg(x):
+    total = 0
+    for i in x:
+        total = total + i
+    return total/len(x)
+
+print avg(x)
+
+def var(x):
+    total = 0
+    av = avg(x)
+    for i in x:
+        total = total + (i-av)*(i-av)
+    return total/len(x-1)
+
+print var(x)
+print numpy.var(x)
+
+def covar(x, y):
+    total = 0
+    avx = avg(x)
+    avy = avg(y)
+    for i, j in zip(x, y):
+        total[0][0] = total + ((i-avx)*(i-avy))
+    return total/len(x)
+
+print covar(x, y)
+print numpy.cov(x, y)
