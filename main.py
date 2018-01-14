e7443bdfd806aac53ef50e3a4a2dbada5957e50d
Vinlagrenouille <vincent.navales@etu.u-pec.fr>
2018-01-14 21:40:04 +0100

    debut de exo 3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ epsi = numpy.random.normal(0, 1, n)
 y = beta0 + (beta1 * x) + epsi
 
 plt.scatter(x,y)
-#plt.show()
+# plt.show()
 
 #EXERCICE 2
 def avg(x):
@@ -24,7 +24,7 @@ def avg(x):
         total = total + i
     return total/len(x)
 
-print avg(x)
+print(avg(x))
 
 def var(x):
     total = 0
@@ -33,16 +33,22 @@ def var(x):
         total = total + (i-av)*(i-av)
     return total/len(x-1)
 
-print var(x)
-print numpy.var(x)
+print(var(x))
+print(numpy.var(x))
 
 def covar(x, y):
     total = 0
     avx = avg(x)
     avy = avg(y)
     for i, j in zip(x, y):
-        total[0][0] = total + ((i-avx)*(i-avy))
+        total = total + ((i-avx)*(i-avy))
     return total/len(x)
 
-print covar(x, y)
-print numpy.cov(x, y)
+print(covar(x, y))
+print(numpy.cov(x, y))
+
+#EXERCICE 3
+
+x[0] = -100
+y[0] = 0
+
